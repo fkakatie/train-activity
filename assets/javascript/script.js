5acbf2ffd9b36a3938049431e116af0254683b51
fkakatie <sextonskatie@gmail.com>
2018-12-11 19:04:47 -0700

    functional form!

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,3 +11,52 @@ messagingSenderId: "825608051446"
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+$('#submitBtn').click(function() {
+    event.preventDefault();
+
+    var trainName = $('#trName').val().trim();
+    var trainDest = $('#trDest').val().trim();
+    var trainFirst = $('#trFirst').val().trim();
+    var trainFreq = $('#trFreq').val().trim();
+
+    // var trainName = $('#trName').val().trim();
+    // var trainName = $('#trName').val().trim();
+    // var trainName = $('#trName').val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDest,
+        firstDeparture: trainFirst,
+        frequency: trainFreq
+    };
+
+    database.ref().push(newTrain);
+
+    $('#trName').val('');
+    $('#trDest').val('');
+    $('#trFirst').val('');
+    $('#trFreq').val('');
+
+});
+
+database.ref().on('child_added',function (childSnapshot) {
+
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var trainFirst = childSnapshot.val().firstDeparture;
+    var trainFreq = childSnapshot.val().frequency;
+
+    var newRow = $('<tr>').append(
+        $('<td>').text(trainName),
+        $('<td>').text(trainDest),
+        $('<td class="centered">').text(trainFreq),
+        $('<td class="centered">').text('Next Arrival'),
+        $('<td class="centered">').text(trainFirst)        
+    );
+
+    $('#trainSched > tbody').append(newRow);
+    
+});
