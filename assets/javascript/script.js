16c2ff3c3c109a876f41c5c6bc5f3df3c999d539
fkakatie <sextonskatie@gmail.com>
2018-12-12 17:55:08 -0700

    full functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,51 +12,78 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$('#submitBtn').click(function() {
-    event.preventDefault();
+$(document).ready(function() {
 
-    var trainName = $('#trName').val().trim();
-    var trainDest = $('#trDest').val().trim();
-    var trainFirst = $('#trFirst').val().trim();
-    var trainFreq = $('#trFreq').val().trim();
+    $('#submitBtn').click(function() {
+        event.preventDefault();
 
-    // var trainName = $('#trName').val().trim();
-    // var trainName = $('#trName').val().trim();
-    // var trainName = $('#trName').val().trim();
+        var trainName = $('#trName').val().trim();
+        var trainDest = $('#trDest').val().trim();
+        var trainFirst = $('#trFirst').val().trim();
+        var trainFreq = $('#trFreq').val().trim();
 
-    var newTrain = {
-        name: trainName,
-        destination: trainDest,
-        firstDeparture: trainFirst,
-        frequency: trainFreq
-    };
+        // var trainName = $('#trName').val().trim();
+        // var trainName = $('#trName').val().trim();
+        // var trainName = $('#trName').val().trim();
 
-    database.ref().push(newTrain);
+        var newTrain = {
+            name: trainName,
+            destination: trainDest,
+            firstDeparture: trainFirst,
+            frequency: trainFreq
+        };
 
-    $('#trName').val('');
-    $('#trDest').val('');
-    $('#trFirst').val('');
-    $('#trFreq').val('');
+        database.ref().push(newTrain);
 
-});
+        $('#trName').val('');
+        $('#trDest').val('');
+        $('#trFirst').val('');
+        $('#trFreq').val('');
 
-database.ref().on('child_added',function (childSnapshot) {
+    });
 
-    console.log(childSnapshot.val());
+    database.ref().on('child_added',function (snap) {
 
-    var trainName = childSnapshot.val().name;
-    var trainDest = childSnapshot.val().destination;
-    var trainFirst = childSnapshot.val().firstDeparture;
-    var trainFreq = childSnapshot.val().frequency;
+        // console.log(snap.val());
 
-    var newRow = $('<tr>').append(
-        $('<td>').text(trainName),
-        $('<td>').text(trainDest),
-        $('<td class="centered">').text(trainFreq),
-        $('<td class="centered">').text('Next Arrival'),
-        $('<td class="centered">').text(trainFirst)        
-    );
+        var trainName = snap.val().name;
+        var trainDest = snap.val().destination;
+        var trainFirst = snap.val().firstDeparture;
+        var trainFreq = snap.val().frequency;
 
-    $('#trainSched > tbody').append(newRow);
-    
+        // var trainTime = moment(trainFirst, 'hh:mm a').format('hh:mm a');
+
+        // console.log(trainTime);
+
+        var firstTrainConvert = moment(trainFirst, "HH:mm");
+        console.log(firstTrainConvert);
+
+        var currentTime = moment();
+        console.log('CURRENT TIME: ' + moment(currentTime).format("hh:mm"));
+
+        var timeBetween = moment().diff(moment(firstTrainConvert), "minutes");
+        console.log('DIFFERENCE: ' + timeBetween);
+
+        var timeApart = timeBetween % trainFreq;
+        console.log('TIME APART: ' + timeApart);
+
+        var minutesAway = trainFreq - timeApart;
+        console.log('MINUTES AWAY: ' + minutesAway);
+
+        var nextArrival = moment().add(minutesAway, "minutes");
+        
+        console.log('NEXT ARRIVAL: ' + nextArrival);
+
+        var newRow = $('<tr>').append(
+            $('<td>').text(trainName),
+            $('<td>').text(trainDest),
+            $('<td class="centered">').text(trainFreq),
+            $('<td class="centered">').text(moment(nextArrival).format('hh:mm a')),
+            $('<td class="centered">').text(minutesAway + ' mins away')        
+        );
+
+        $('#trainSched > tbody').append(newRow);
+        
+    });
+
 });
