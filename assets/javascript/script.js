9448b03d84a4147e30cedeeb216df31a1f8e0ed9
fkakatie <sextonskatie@gmail.com>
2018-12-14 23:32:10 -0700

    design and media queries

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,76 +14,110 @@ var database = firebase.database();
 
 $(document).ready(function() {
 
+    // color loop image and modal header
+    setInterval(function setHue() {
+        var hueColor = Math.floor(Math.random() * 360) + 1;
+        var hueDeg = "hue-rotate(" + hueColor + "deg)";
+
+        $('#trPic').css({
+            "filter": hueDeg, 
+            "transition": "5s all"
+        });
+
+        $('.modal-header').css({
+            "filter": hueDeg,
+            "transition": "5s all"
+        });
+
+    }, 5000);
+
     $('#submitBtn').click(function() {
         event.preventDefault();
 
+        // collect data from form and trim
         var trainName = $('#trName').val().trim();
         var trainDest = $('#trDest').val().trim();
         var trainFirst = $('#trFirst').val().trim();
         var trainFreq = $('#trFreq').val().trim();
 
-        // var trainName = $('#trName').val().trim();
-        // var trainName = $('#trName').val().trim();
-        // var trainName = $('#trName').val().trim();
+        // execute if form is fully completed
+        if (trainName !== '' && trainDest !== '' && trainFirst !== '' && trainFreq !== '') {
 
-        var newTrain = {
-            name: trainName,
-            destination: trainDest,
-            firstDeparture: trainFirst,
-            frequency: trainFreq
-        };
+            // store form data in object
+            var newTrain = {
+                name: trainName,
+                destination: trainDest,
+                firstDeparture: trainFirst,
+                frequency: trainFreq
+            };
 
-        database.ref().push(newTrain);
+            // push new object to firebase
+            database.ref().push(newTrain);
 
-        $('#trName').val('');
-        $('#trDest').val('');
-        $('#trFirst').val('');
-        $('#trFreq').val('');
+            // clear form
+            $('#trName').val('');
+            $('#trDest').val('');
+            $('#trFirst').val('');
+            $('#trFreq').val('');
+
+        }
 
     });
-
+    
+    // execute for all child data in firebase
     database.ref().on('child_added',function (snap) {
 
         // console.log(snap.val());
 
+        // grab data from firebase
         var trainName = snap.val().name;
         var trainDest = snap.val().destination;
         var trainFirst = snap.val().firstDeparture;
         var trainFreq = snap.val().frequency;
 
-        // var trainTime = moment(trainFirst, 'hh:mm a').format('hh:mm a');
-
-        // console.log(trainTime);
-
-        var firstTrainConvert = moment(trainFirst, "HH:mm");
-        console.log(firstTrainConvert);
+        // set first train back one year to ensure it falls before current time
+        var firstTrainConvert = moment(trainFirst, "hh:mm").subtract(1, "years");
 
         var currentTime = moment();
-        console.log('CURRENT TIME: ' + moment(currentTime).format("hh:mm"));
 
+        // calculate time between current time and first train
         var timeBetween = moment().diff(moment(firstTrainConvert), "minutes");
-        console.log('DIFFERENCE: ' + timeBetween);
 
+        // divide timeBetween by frequency of trains
         var timeApart = timeBetween % trainFreq;
-        console.log('TIME APART: ' + timeApart);
 
+        // subtract train frequency by timeApart
         var minutesAway = trainFreq - timeApart;
-        console.log('MINUTES AWAY: ' + minutesAway);
 
+        // calculate next arrival
         var nextArrival = moment().add(minutesAway, "minutes");
         
-        console.log('NEXT ARRIVAL: ' + nextArrival);
-
+        // create new row to hold train data
         var newRow = $('<tr>').append(
             $('<td>').text(trainName),
             $('<td>').text(trainDest),
-            $('<td class="centered">').text(trainFreq),
+            $('<td class="centered">').text('every ' + trainFreq + ' mins'),
             $('<td class="centered">').text(moment(nextArrival).format('hh:mm a')),
             $('<td class="centered">').text(minutesAway + ' mins away')        
         );
 
+        // add new train row to table body
         $('#trainSched > tbody').append(newRow);
         
+    });
+
+    // setup modal
+    var modal = $('#myModal');
+
+    // when the user clicks the button, open the modal 
+    $('#trBtn').click(function() {
+        event.preventDefault();
+        modal.css("display", "block");
+    });
+
+    // when the user clicks x, close the modal
+    $('.close').click(function() {
+        modal.css("display", "none");
     });
 
 });
